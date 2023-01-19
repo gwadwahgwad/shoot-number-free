2737431d103df4005eafb7cf3601dbe465a5cc9b
DisconnectZc <122740188+gwadwahgwad@users.noreply.github.com>
2023-01-20 02:37:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> 
+    </title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form autocomplete="off">
+
+<h1 class="text-center">เอาไปใช้ฟรี</h1>
+                  <h2 class="text-center"></h2>
+                    <h3 class="text-center"></h3>
+                    
+                        <div class="alert alert-success text-center">
+                            <p>DisconnectZc</p>
+                          <p id="status">Status : shoot number</p>
+                          <p id="done"></p>
+                        </div>
+                    <div class="form-group">
+                        <input class="form-control" id="phone" type="number" name="otp" placeholder="เบอร์" required>
+                      <input class="form-control" id="Amount" type="number" name="otp" placeholder="จำนวน" required>
+                    </div>
+                    <div class="form-group">
+                        <button class="form-control button" id="spam" type="button">กดเพื่อยิงเบอร์</button>
+                        <script src="index.js">
+                        </script>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
